3cac92c387095473311c0efe071817e6c2e7222b
Saim-Akhtar <saimakhtar98@gmail.com>
2024-03-17 16:54:30 +0200

    added secret auth

--- admin-app/tracker_app/views.py
+++ admin-app/tracker_app/views.py
@@ -4,13 +4,36 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from .models import Action, Parcel
 from django.core.serializers import serialize
+from django.conf import settings
 
 
 
+def custom_auth_required(view_func):
+    """
+    Decorator to enforce custom authentication based on security key.
+    """
+    def _wrapped_view(request, *args, **kwargs):
+        # Check if the security key is provided in the request headers or parameters
+        security_key = request.headers.get('X-Security-Key')  # Assuming security key is provided in headers
+        if not security_key:
+            # Return unauthorized response if security key is missing
+            return JsonResponse({'error': 'Unauthorized'}, status=401)
+
+        # Check if the provided security key is valid
+        valid_keys = settings.SECRET_KEY  # Replace 'your_valid_key_here' with your actual valid security key
+        if security_key not in valid_keys:
+            # Return unauthorized response if the key is invalid
+            return JsonResponse({'error': 'Unauthorized'}, status=401)
+
+        # Execute the original view function with parameters
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
 
 # Create your views here.
 
 @require_http_methods(["GET"])
+@custom_auth_required
 def get_tracking_history(request, tracking_id):
     try:
         parcel = Parcel.objects.prefetch_related('actions').get(tracking_id=tracking_id)

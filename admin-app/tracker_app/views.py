b6a897043c5bdbbd48ea900ef88d01159356accb
Saim-Akhtar <saimakhtar98@gmail.com>
2024-03-17 16:19:47 +0200

    added admin changes and api endpoint

--- admin-app/tracker_app/views.py
+++ admin-app/tracker_app/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .models import Action
+
+
+
 
 # Create your views here.
+
+@require_http_methods(["GET"])
+def get_tracking_history(request, tracking_id):
+    history = list(Action.objects.filter(parcel__tracking_id=tracking_id))
+    return JsonResponse({
+        "history": history
+    })

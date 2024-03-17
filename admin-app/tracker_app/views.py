3575a65edde57c6c689cd60110110bb3d6a1640d
Saim-Akhtar <saimakhtar98@gmail.com>
2024-03-17 16:40:22 +0200

    fix endpoint

--- admin-app/tracker_app/views.py
+++ admin-app/tracker_app/views.py
@@ -1,7 +1,9 @@
+import json
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from .models import Action
+from .models import Action, Parcel
+from django.core.serializers import serialize
 
 
 
@@ -10,7 +12,26 @@ from .models import Action
 
 @require_http_methods(["GET"])
 def get_tracking_history(request, tracking_id):
-    history = list(Action.objects.filter(parcel__tracking_id=tracking_id))
-    return JsonResponse({
-        "history": history
-    })
+    try:
+        parcel = Parcel.objects.prefetch_related('actions').get(tracking_id=tracking_id)
+    except Parcel.DoesNotExist:
+        return JsonResponse({'error': 'Parcel not found'}, status=404)
+
+    print(parcel)   
+
+    serialized_actions = serialize('json', parcel.actions.all())
+
+    actions = json.loads(serialized_actions)
+
+
+    parcel_data = {
+        'id': parcel.id,
+        'tracking_id': parcel.tracking_id,
+        'user_email': parcel.user_email,
+        'detail': parcel.detail,
+        'created_at': parcel.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+        'departure': parcel.departure,
+        'destination': parcel.destination,
+        'actions': actions,
+    }
+    return JsonResponse(parcel_data, safe=False)
